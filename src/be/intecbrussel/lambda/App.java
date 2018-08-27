5570cf36e3c99cd7cdaf13de715c86eba27c7dff
intec <intec@intec.intecbrussel.be>
2018-08-27 10:08:21 +0200

    updated

--- src/be/intecbrussel/lambda/App.java
+++ src/be/intecbrussel/lambda/App.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.lambda;
 
+import java.util.function.Function;
+
 public class App {
 
 	public static void main(String[] args) {
@@ -7,21 +9,23 @@ public class App {
 		TextPrinter text = new TextPrinter(
 				"Give a man a gun and he can rob a bank, but give a man a bank, and he can rob the world.");
 
+		Function<String, String> func1 = TextUtil::reverse;
+
 		System.out.println("########################" + "\nReverse Method\n" + "########################");
 
 		text.printProcessedWords(s -> TextUtil.reverse(s));
 		text.printProcessedWords(TextUtil::reverse);
 
-		System.out.println("########################" + "\nScrambler Method\n" + "########################");
+		System.out.println("\n\n########################" + "\nScrambler Method\n" + "########################");
 		TextScrambler scrambler = new TextScrambler();
 		text.printProcessedWords(s -> scrambler.scramble(s));
 		text.printProcessedWords(scrambler::scramble);
 
-		System.out.println("########################" + "\nContains Method\n" + "########################");
+		System.out.println("\n\n########################" + "\nContains Method\n" + "########################");
 		text.printFilteredWords(s -> s.contains("e"));
 
-		System.out.println("########################" + "\nLong\n" + "########################");
-		//text.printNumberValues(s -> new Long(s));
+		System.out.println("\n\n########################" + "\nLong\n" + "########################");
+		// text.printNumberValues(s -> new Long(s));
 	}
 
 }
--- src/be/intecbrussel/lambda/TextPrinter.java
+++ src/be/intecbrussel/lambda/TextPrinter.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.lambda;
 
+import java.util.function.Function;
+
 public class TextPrinter {
 	private String something;
 
@@ -10,25 +12,31 @@ public class TextPrinter {
 
 	public void printProcessedWords(WordProcessor processor) {
 		for (String r0b0t : something.split(" ")) {
-	         System.out.println(processor.process(r0b0t));
-	      }
-		
+			System.out.print(processor.process(r0b0t));
+		}
+
 	}
 
 	public String toString() {
 		return something;
 	}
-
+	
+	
+	
 	public void printFilteredWords(WordFilter filter) {
 		for (String w : something.split(" ")) {
 			if (filter.isValid(w)) {
-				System.out.println(w);
+				System.out.print(w);
 			}
 		}
 	}
+	 
 
-	
-
-	
+	public void printProcessedWords(Function<String, String> processor) {
+		for (String w : something.split(" ")) {
+			String processedWord = processor.apply(w);
+			System.out.println(processedWord);
+		}
+	}
 
 }
